d9e094bf2b613087024be8534b900ab0daee0959
NICOLE ALICE GOMES BRANCO <nicole.agbranco@senacsp.edu.br>
2022-05-13 11:54:16 -0300

    Criação de produtos

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,14 +1,57 @@
-import {useState} from 'react'
+import {useState} from "react";
 
 function App() {
    
-     const [n,setN] = useState(1);
+     const [produto, setProduto] = useState([
+       
+      {
+         id:500,
+         nome: "Mouse",
+         categoria: "Informática",
+         quantidade:15,
+         preco:50.00
+       },
+       
+       {
+         id:501,
+         nome: "Teclado",
+         categoria: "Informática",
+         quantidade:20,
+         preco:150.90
+       }
+     ]);
   
   
    return (
    <div>
-    <p>{n}</p>
-    <button onClick={()=>setN(n+1)}>Clique</button>
+       <button onClick={()=>{
+           var cod = prompt("Entre com o código");
+           var no = prompt("Entre com o nome do produto");
+           var cat = prompt("Entre com a categoria");
+           var qt = prompt("Entre com a quantidade");
+           var pr = prompt("Entre com o preco");
+         
+         setProduto(
+           produto.concat({
+            id:cod,
+            nome: no,
+            categoria: cat,
+            quantidade:qt,
+            preco: pr,
+       }))}}>
+       Clique
+       </button>
+
+     {produto.map((item,index) =>(
+        <div key={index}>
+        <h2>{item.nome}</h2>
+        <ul>
+          <li>categoria: {item.categoria}</li>
+          <li>quantidade: {item.quantidade}</li>
+          <li>produto: {item.preco}</li>
+        </ul>
+        </div>
+     ))};
    </div>
   );
 }
